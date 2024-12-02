e9a93bf0fbe295fb96480978e50996dcb43d0b2c
Shardendu Mishra <shardendumishra01@gmail.com>
2024-12-02 21:30:34 +0530

    Made My Main Template for CP in C++.

--- CPP_CP_DSA_Template.cpp
+++ CPP_CP_DSA_Template.cpp
@@ -1,14 +1,53 @@
 #include<bits/stdc++.h>;
 using namespace std;
 
+#define c_int const int
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define nptr nullptr
+#define endl "\n"
+
+#define YES cout<<"Yes \n";
+#define No cout<<"No \n";
 #define PINF INT_MAX
 #define NINF INT_MIN
 
-#define ll long long;
-#define pb push_back;
-#define mp make_pair;
+typedef priority_queue<long long,vector<long long>,greater<long long>> prq_ll;
+typedef priority_queue<int,vector<int>,greater<int>> prq_int;
+typedef double dbl;
 
-#define vector<ll> vll;
-#define vector<bool> vbl;
-#define vector<int> vint;
-#define vector<string> vstr;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<string>> vvstr;
+typedef vector<vector<bool>> vvbl;
+typedef vector<vector<int>> vvint;
+
+typedef vector<long long> vll;
+typedef vector<string> vstr;
+typedef vector<int> vint;
+typedef vector<bool> vbl;
+
+c_int MOD = 1e9 + 7;
+c_int Mx_row = 100;
+c_int Mx_col = 100;
+
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void solve();
+
+int main(){
+    fast();
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+void solve(){
+    
+}
