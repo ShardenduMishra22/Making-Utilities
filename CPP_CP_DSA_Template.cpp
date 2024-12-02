c43c50cf88b78f5d1b8e0078f5585e8dcf7d4f59
Shardendu Mishra <shardendumishra01@gmail.com>
2024-12-02 21:19:20 +0530

    first commit

---
+++ CPP_CP_DSA_Template.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+#define PINF INT_MAX
+#define NINF INT_MIN
+
+#define ll long long;
+#define pb push_back;
+#define mp make_pair;
+
+#define vector<ll> vll;
+#define vector<bool> vbl;
+#define vector<int> vint;
+#define vector<string> vstr;
